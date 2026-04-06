674fafbe3fe01c024abe13030fdb564fb9430f7c
Recyda GPG <patrick.felber@recyda.com>
2026-04-06 02:21:27 +0200

    More status

--- src/config/devices.js
+++ src/config/devices.js
@@ -30,12 +30,39 @@ export const rooms = ['Main', 'Bedroom', 'Kitchen']
 // Any other key is shown as-is.
 export const devices = [
   {
-    id: 'main-light',
-    name: 'Offroadleuchten Hinten',
+    id: 'trunk_fan',
+    name: 'Trunk Fan',
+    room: 'Main',
+    type: 'switch',
+    topic: 'zigbee2mqtt-frap/FRAP - Kofferraum Switch',
+    commandTopic: 'zigbee2mqtt-frap/FRAP - Kofferraum Switch/l1/set',
+    stateKey: 'state_l1',
+  },
+  {
+    id: 'dachluefter',
+    name: 'Maxx Fan',
+    room: 'Main',
+    type: 'switch',
+    topic: 'zigbee2mqtt-frap/FRAP - Kofferraum Switch',
+    commandTopic: 'zigbee2mqtt-frap/FRAP - Kofferraum Switch/l2/set',
+    stateKey: 'state_l2',
+  },
+  {
+    id: 'offroad_lights_hl',
+    name: 'Offroadbeleuchtung Hinten Link',
     room: 'Main',
     type: 'switch',
     topic: 'zigbee2mqtt-frap/FRAP - Offroadleuchten Hinten Switch',
-    commandTopic: 'zigbee2mqtt-frap/FRAP - Offroadleuchten Hinten Switch/l3/set',
-    stateKey: 'state_l3',
+    commandTopic: 'zigbee2mqtt-frap/FRAP - Offroadleuchten Hinten Switch/l1/set',
+    stateKey: 'state_l1',
+  },
+  {
+    id: 'offroad_lights_hr',
+    name: 'Offroadbeleuchtung Hinten Rechts',
+    room: 'Main',
+    type: 'switch',
+    topic: 'zigbee2mqtt-frap/FRAP - Offroadleuchten Hinten Switch',
+    commandTopic: 'zigbee2mqtt-frap/FRAP - Offroadleuchten Hinten Switch/l2/set',
+    stateKey: 'state_l2',
   }
 ]
