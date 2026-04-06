46889f1cf6ed8ae18b8c0e60b3ec68e770f21349
Recyda GPG <patrick.felber@recyda.com>
2026-04-06 03:27:27 +0200

    More sensors

--- src/config/devices.js
+++ src/config/devices.js
@@ -28,6 +28,16 @@ export const devices = [
     icon: 'fan'
   },
   {
+    id: 'trunk_light',
+    name: 'Trunk Lights',
+    room: 'Trunk',
+    type: 'switch',
+    topic: 'zigbee2mqtt-frap/FRAP - Kofferraum Switch',
+    commandTopic: 'zigbee2mqtt-frap/FRAP - Kofferraum Switch/l3/set',
+    stateKey: 'state_l3',
+    icon: 'lightbulb'
+  },
+  {
     id: 'offroad_lights_hl',
     name: 'Offroadbeleuchtung Hinten Link',
     room: 'Outside',
@@ -112,4 +122,146 @@ export const devices = [
     },
     icon: 'thermometer'
   },
+
+
+
+  { 
+    id: 'smartshunt_battery_voltage',
+    name: 'Smart Shunt Battery Voltage',
+    room: 'Living Room',
+    type: 'sensor', 
+    topic: 'homeassistant/sensor/smartshunt_c615f5e0ebea/battery_voltage/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'Voltage', unit: 'V', format: v => Number(v).toFixed(1) },
+    },
+    icon: 'battery'
+  },
+  {
+    id: 'smartshunt_current',
+    name: 'Smart Shunt Current',
+    room: 'Living Room',
+    type: 'sensor',
+    topic: 'homeassistant/sensor/smartshunt_c615f5e0ebea/current/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'Current', unit: 'A', format: v => Number(v).toFixed(1) },
+    },
+    icon: 'battery'
+  },
+  {
+    id: 'smartshunt_remainingminutes',        
+    name: 'Smart Shunt Remaining Minutes',        
+    room: 'Living Room',
+    type: 'sensor',
+    topic: 'homeassistant/sensor/smartshunt_c615f5e0ebea/remainingminutes/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'Minutes', unit: 'Min.', format: v => Number(v).toFixed(0) },
+    },
+    icon: 'battery'
+  },
+  { 
+    id: 'smartshunt_soc',
+    name: 'Smart Shunt State Of Charge',
+    room: 'Living Room',
+    type: 'sensor',
+    topic: 'homeassistant/sensor/smartshunt_c615f5e0ebea/soc/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'State of Charge', unit: '%', format: v => Number(v).toFixed(1) },
+    },
+    icon: 'battery'
+  },
+  { 
+    id: 'smartshunt_consumedah',             
+    name: 'Smart Shunt Consumed Ah',  
+    room: 'Living Room',
+    type: 'sensor',
+    topic: 'homeassistant/sensor/smartshunt_c615f5e0ebea/consumedah/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'Consumed Ah', unit: 'Ah', format: v => Number(v).toFixed(1) },
+    },
+    icon: 'battery'
+  },
+
+
+
+
+
+  { 
+    id: 'mppt_state',
+    name: 'MPPT State',  
+    room: 'Living Room',
+    type: 'sensor',
+    topic: 'homeassistant/sensor/mppt_c8526f13b1c1/charge_state/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'MPPT State', unit: '', format: v => v },
+    },
+    icon: 'sun'
+  },
+  {
+    id: 'mppt_charger_error',
+    name: 'MPPT Charger Error', 
+    room: 'Living Room',  
+    type: 'sensor',
+    topic: 'homeassistant/sensor/mppt_c8526f13b1c1/charger_error/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'Charger Error', unit: '', format: v => v },
+    },
+    icon: 'sun'
+  },
+  { 
+    id: 'mppt_battery_voltage',
+    name: 'MPPT Battery Voltage',
+    room: 'Living Room',
+    type: 'sensor',
+    topic: 'homeassistant/sensor/mppt_c8526f13b1c1/battery_voltage/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'Voltage', unit: 'V', format: v => Number(v).toFixed(1) },
+    },
+    icon: 'battery'
+  },
+  { 
+    id: 'mppt_battery_charging_current',
+    name: 'MPPT Battery Charging Current',
+    room: 'Living Room',
+    type: 'sensor',
+    topic: 'homeassistant/sensor/mppt_c8526f13b1c1/battery_charging_current/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'Current', unit: 'A', format: v => Number(v).toFixed(1) },
+    },
+    icon: 'battery'
+  },
+  { 
+    id: 'mppt_power',
+    name: 'MPPT Solar Power',
+    room: 'Living Room',
+    type: 'sensor',
+    topic: 'homeassistant/sensor/mppt_c8526f13b1c1/solar_power/state',
+    payloadKey: 'value',
+    properties: ['value'],
+    propertyMeta: {
+      value: { label: 'Power', unit: 'W', format: v => Number(v).toFixed(1) },
+    },
+    icon: 'battery'
+  },
+
+
+
+
 ]
